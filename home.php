d0eab845aa6f053c88a7511c334378ab6a863aa0
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2020-10-23 16:56:39 +0400

    First Commit

---
+++ home.php
@@ -0,0 +1,54 @@
+<?php
+
+$pw_string =array();
+$pw_string[] = "O";
+$pw_string[] = "T";
+$pw_string[] = "E";
+$pw_string[] = "z";
+$pw_string[] = "O";
+$pw_string[] = "D";
+$pw_string[] = "Q";
+$pw_string[] = "0";
+$pw_string[] = "e";
+$pw_string[] = "W";
+$pw_string[] = "l";
+$pw_string[] = "o";
+$pw_string[] = "d";
+$pw_string[] = "D";
+$pw_string[] = "V";
+$pw_string[] = "1";
+$pw_string[] = "Z";
+$pw_string[] = "m";
+$pw_string[] = "8";
+$pw_string[] = "0";
+$pw_string[] = "d";
+$pw_string[] = "w";
+$pw_string[] = "F";
+$pw_string[] = "i";
+$pw_string[] = "g";
+$pw_string[] = "4";
+$pw_string[] = "0";
+$pw_string[] = "D";
+$pw_string[] = "0";
+$pw_string[] = "X";
+$pw_string[] = "E";
+$pw_string[] = "A";
+$pw_string[] = "T";
+$pw_string[] = "d";
+$pw_string[] = "P";
+$pw_string[] = "x";
+$pw_string[] = "i";
+$pw_string[] = "O";
+$pw_string[] = "e";
+$pw_string[] = "c";
+$pw_string[] = "R";
+$pw_string[] = "B";
+$pw_string[] = "Q";
+$pw_string[] = "=";
+var_dump($pw_string);
+$numberstring = array(10,14,5,3,2);
+
+foreach($numberstring as $no){
+	echo ''.$pw_string[$no];
+}
+?>
